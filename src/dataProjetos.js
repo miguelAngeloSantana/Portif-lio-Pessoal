3d26b15fd34978b3b7fd078df8542a3040aee08f
Miguel Ângelo <65346317+miguelAngeloSantana@users.noreply.github.com>
2023-12-15 22:51:46 -0300

    Update dataProjetos.js
    ajustando esse componente para ele poder renderizar as imagens

--- src/dataProjetos.js
+++ src/dataProjetos.js
@@ -1,10 +1,16 @@
+import imgProject1 from "./Imagens/project-1.png";
+import imgProject2 from "./Imagens/project-2.png";
+import imgProject3 from "./Imagens/project-3.png";
+import imgProject4 from "./Imagens/project-4.png";
+
+
 export const Projects = [
     {
         title: "Spa Doorhub",
         tec: "React",
         desc: "Este projeto é uma Single Page Application de um rede Fast Food ficticia, sendo feita com react, além da incorporaçao de algumas ilustrações em algumas sessões para deiar o visual mais informativo",
         link: "https://spadoorhub.netlify.app/",
-        image: "./Imagens/project-1.png"
+        image: imgProject1
     },
 
     {
@@ -12,7 +18,7 @@ export const Projects = [
         tec: "HTML, CSS, JavaScript",
         desc: "Nesse projeto eu fiz um quadro kanbam, onde é possivel criar quantos cartões quiser em cada uma das três coluns. Além de ser possivel mover um cartao de uma coluna para outra",
         link: "https://projectquadrokanbam.netlify.app/",
-        image: "./src/Imagens/project-2.png",
+        image: imgProject2
     },
 
     {
@@ -20,7 +26,7 @@ export const Projects = [
         tec: "FlexBox, Grid e FireBase",
         desc: "Nesse projeto, eu crio um site inspirado no energético da Monster. Sendo uso do Flexbox e do Css Gri para ajudar na estilizaçao de algumas sessões do site. Além da integraçao de um formulario com o banco de dados da FireBase",
         link: "https://website-monster.netlify.app/",
-        image: "./src/Imagens/project-3.png"
+        image: imgProject3
     },
 
     {
@@ -28,6 +34,6 @@ export const Projects = [
         tec: "HTML, CSS, JavaScript",
         desc: "Nesse projeto eu fiz uma landing page, onde busco fazer algumas modificações para ajustar o layout aos diferentes tamanhos de tela, como a utilizaçaõ de um slidershow para melhorar a visualizaçao nas telas de mobile",
         link: "https://miguel-manage-landing-page.netlify.app/",
-        image: "./src/Imagens/project-4.png"
+        image: imgProject4
     }
 ]
